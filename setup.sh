e2882d0a5c0d018c7d9bcd66c4070a7cc5d1145f
ASTOHACKER <50506200+ASTOHACKER@users.noreply.github.com>
2019-05-11 08:51:28 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,70 @@
+g='\033[1;32m'
+
+p='\033[1;36m'
+
+cd
+clear
+
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.4
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+sleep 0.4
